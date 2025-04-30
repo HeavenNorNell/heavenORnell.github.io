2bb328ad6e43721ae2b0342151c7d61741b95f9f
asp2131 <akinpdsgmail.com>
2025-04-29 19:44:53 -0500

    commit

--- iot-projects/first-slice-of-pi/wot-server.js
+++ iot-projects/first-slice-of-pi/wot-server.js
@@ -1,10 +1,17 @@
 const httpServer = require('./servers/http'),
-	resources = require('./resources/model');
+	resources = require('./resources/model'),
+	pirPlugin = require("./plugins/internal/pirPlugin");
+	dhtPlugin = require("./plugins/internal/dhtPlugin");
+
+pirPlugin.start({});
+dhtPlugin.start({'frequency': 2000});
 
 const server = httpServer.listen(resources.pi.port, function () {
 	console.log("Running the Pi on port " + resources.pi.port);
 });
 
-process.on('SIGINT', function() {
+process.on('SIGINT', function () {
+	pirPlugin.stop();
+	dhtPlugin.stop();
 	process.exit();
 });
