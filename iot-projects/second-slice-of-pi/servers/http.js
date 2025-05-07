9463e120d3272ce31329dd60def329a43a9e6b0f
asp2131 <akinpdsgmail.com>
2025-05-06 19:27:04 -0500

    finished second slice

--- iot-projects/second-slice-of-pi/servers/http.js
+++ iot-projects/second-slice-of-pi/servers/http.js
@@ -1,11 +1,15 @@
+var converter = require('./../middleware/converter');
+var bodyParser = require('body-parser');
 var sensorRoutes = require('./../routes/sensors');
 var actuatorRoutes = require('./../routes/actuators');
 const express = require('express'),
 	cors = require('cors');
 var app = express();
+app.use(bodyParser.json());
 app.use(cors());
 app.use('/pi/sensors', sensorRoutes);
 app.use('/pi/actuators', actuatorRoutes);
+app.use(converter());
 
 app.get("/", function (req, res) {
 	res.send("Welcome to the root! Climb the tree?");
@@ -14,6 +18,7 @@ app.get("/", function (req, res) {
 app.get("/pie", function (req, res) {
 	res.send("Welcome to the gate! Enter the garden?");
 });
+
 
 module.exports = app;
 // I have looked through all the files
