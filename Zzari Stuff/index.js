7f634102fe29adda9febb9b24c58fe9eaa468e38
Helen Rogers <ebonytheduskwing@gmail.com>
2025-05-20 22:27:28 +0000

    fixed branching commits

--- Zzari Stuff/index.js
+++ Zzari Stuff/index.js
@@ -1,7 +1,7 @@
-
+document.addEventListener("DOMContentLoaded", function() {
 const pilot = [
-    document.getElementById("pilot")
-    // li: document.getElementById("pilotLi")
+  document.getElementById("pilot"),
+  document.getElementById("pilotLi")
 ];
 //   const lore = {
 //   main: document.getElementById("lore"),
@@ -15,33 +15,40 @@ const pilot = [
 //   main: document.getElementById("languages"),
 //     li: document.getElementById("languagesLi")
 //   }
-  
-  setupDropdown(pilot);
+
+setupDropdown(pilot);
 //   setupDropdown(lore);
 //   setupDropdown(conceptArt);
 //   setupDropdown(languages);
-  
-  function setupDropdown(ID){
-    colorChange(ID[0], "#315e31");
-    ID[0].addEventListener("mouseover", function (){ colorChange(ID[0], "green");})
-    ID[0].addEventListener("mouseout", function (){ colorChange(ID[0], "#e6d7a1");})
-    ID[0].addEventListener("click", function (){ dropDown(ID[0]);})
-    ID.li.style.display = "block";
-    ID[0].isOn = false;
+
+function setupDropdown(ID) {
+  colorChange(ID[0], "#315e31");
+  ID[0].addEventListener("mouseover", function () {
+    colorChange(ID[0], "green");
+  });
+  ID[0].addEventListener("mouseout", function () {
+    colorChange(ID[0], "#e6d7a1");
+  });
+  ID[0].addEventListener("click", function () {
+    dropDown(ID);
+  });
+  ID[1].style.display = "none";
+  ID[1].isOn = false;
+}
+
+function colorChange(ID, color) {
+  ID.style.color = color;
+}
+
+function dropDown(ID) {
+  if (ID[1].isOn) {
+    ID[0].innerHTML = ID[0].innerHTML.replace("⮝", "⮟");
+    ID[1].style.display = "none";
+    ID[1].isOn = false;
+  } else {
+    ID[0].innerHTML = ID[0].innerHTML.replace("⮟", "⮝");
+    ID[1].style.display = "block";
+    ID[1].isOn = true;
   }
-  
-  function colorChange(ID, color){
-    ID.style.color = color;
-  }
-  
-  function dropDown(ID){
-    if(ID.isOn){
-    ID[0].innerHTML = ID.innerHTML.replace("⮝", "⮟");
-    ID.li.style.display = "block";
-    ID.isOn = false;
-    } else {
-    ID[0].innerHTML = ID.innerHTML.replace("⮟", "⮝"); 
-        ID.li.style.display = "none";
-    ID[0].isOn = true;
-    }
-  }
+}
+});
