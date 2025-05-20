bba259df7c054e676b15dc65fc875629d0bd0077
Helen Rogers <ebonytheduskwing@gmail.com>
2025-05-20 23:14:12 +0000

    push code

--- Zzari Stuff/index.js
+++ Zzari Stuff/index.js
@@ -1,54 +1,60 @@
-document.addEventListener("DOMContentLoaded", function() {
-const pilot = [
-  document.getElementById("pilot"),
-  document.getElementById("pilotLi")
-];
-//   const lore = {
-//   main: document.getElementById("lore"),
-//     li: document.getElementById("loreLi")
-//   };
-//   const conceptArt = {
-//   main: document.getElementById("conceptArt"),
-//     li: document.getElementById("conceptArtLi")
-//   };
-//   const languages = {
-//   main: document.getElementById("languages"),
-//     li: document.getElementById("languagesLi")
-//   }
+document.addEventListener("DOMContentLoaded", function () {
+  const dropDowns = {
+    pilot: [
+      document.getElementById("pilot"),
+      document.getElementById("pilotLi")
+    ],
+    lore: [
+      document.getElementById("lore"),
+      document.getElementById("loreLi")
+    ],
+    conceptArt: [
+      document.getElementById("conceptArt"),
+      document.getElementById("conceptArtLi")
+    ],
+    languages: [
+      document.getElementById("languages"),
+      document.getElementById("languagesLi")
+    ]
+  }
+  setupDropdown(dropDowns.pilot);
+  setupDropdown(dropDowns.lore);
+  setupDropdown(dropDowns.conceptArt);
+  setupDropdown(dropDowns.languages);
 
-setupDropdown(pilot);
-//   setupDropdown(lore);
-//   setupDropdown(conceptArt);
-//   setupDropdown(languages);
-
-function setupDropdown(ID) {
-  colorChange(ID[0], "#315e31");
-  ID[0].addEventListener("mouseover", function () {
-    colorChange(ID[0], "green");
-  });
-  ID[0].addEventListener("mouseout", function () {
-    colorChange(ID[0], "#e6d7a1");
-  });
-  ID[0].addEventListener("click", function () {
-    dropDown(ID);
-  });
-  ID[1].style.display = "none";
-  ID[1].isOn = false;
-}
-
-function colorChange(ID, color) {
-  ID.style.color = color;
-}
-
-function dropDown(ID) {
-  if (ID[1].isOn) {
-    ID[0].innerHTML = ID[0].innerHTML.replace("⮝", "⮟");
+  function setupDropdown(ID) {
+    ID[0].addEventListener("mouseover", function () {
+      colorChange(ID[0], "green");
+    });
+    ID[0].addEventListener("mouseout", function () {
+      colorChange(ID[0], "#e6d7a1");
+    });
+    ID[0].addEventListener("click", function () {
+      dropDown(ID);
+    });
     ID[1].style.display = "none";
     ID[1].isOn = false;
-  } else {
-    ID[0].innerHTML = ID[0].innerHTML.replace("⮟", "⮝");
-    ID[1].style.display = "block";
-    ID[1].isOn = true;
   }
-}
+
+  function colorChange(ID, color) {
+    ID.style.color = color;
+  }
+
+  function dropDown(ID) {
+    if (ID[1].isOn) {
+      ID[0].innerHTML = ID[0].innerHTML.replace("⮝", "⮟");
+      ID[1].style.display = "none";
+      ID[1].isOn = false;
+    } else {
+      for (var key in dropDowns) {
+        // if(dropDowns[key][1].isOn === true){
+        dropDowns[key][1].style.display = "none";
+        dropDowns[key][1].isOn = false;
+        // }
+        ID[0].innerHTML = ID[0].innerHTML.replace("⮟", "⮝");
+        ID[1].style.display = "block";
+        ID[1].isOn = true;
+      }
+    }
+  }
 });
